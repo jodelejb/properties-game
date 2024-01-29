d1a61f1232402725ba342145afdda3c55dd625ff
Justas Jodele <jodele.justas@gmail.com>
2024-01-28 16:58:24 -0800

    Grav cannon added to game

--- scenes/properties/hold_manager.gd
+++ scenes/properties/hold_manager.gd
@@ -7,9 +7,15 @@ var held_object: PhysicsBody3D:
 		return held_object
 	set(value):
 		if held_object == value: return
-		#var old_held_object = held_object
-		if held_object != null: held_object.hm.holder = null
+		var old_held_object = held_object
 		held_object = value
+		if held_object != null:
+			print(phys_body.name + "'s held object is " + held_object.name)
+		else:
+			print(phys_body.name + "'s held object is null")
+		if old_held_object != null: 
+			if old_held_object.hm.holder == phys_body:
+				old_held_object.hm.holder = null
 		if held_object != null: held_object.hm.holder = phys_body
 
 var holder: PhysicsBody3D:
@@ -17,12 +23,19 @@ var holder: PhysicsBody3D:
 		return holder
 	set(value):
 		if holder == value: return
-		#if holder != null: holder.hm.held_object = phys_body
+		var old_holder = holder
 		holder = value
+		if holder != null:
+			print(phys_body.name + "'s holder is " + holder.name)
+		else:
+			print(phys_body.name + "'s holder is null")
+		if old_holder != null: 
+			if old_holder.hm.held_object == phys_body:
+				old_holder.hm.held_object = null
 		if holder != null: holder.hm.held_object = phys_body
-		print("holder set to " + str(holder))
+		#print("holder set to " + str(holder))
 
-@export var held_object_speed: float = 10.0
+@export var held_object_speed: float = 800
 @export var throw_speed: float = 15.0
 @export var hold_point: Marker3D
 
@@ -30,7 +43,7 @@ func _ready():
 	phys_body.add_to_group("Holding")
 
 func _physics_process(delta):
-	follow()
+	follow(delta)
 
 func pick_up(collider):
 	if held_object != null:
@@ -45,13 +58,21 @@ func pick_up(collider):
 func throw(throw_vec: Vector3):
 	if held_object == null: return
 	#var look_vec = (holding.global_position - head.global_position).normalized()
-	var obj_speed = held_object.linear_velocity.length()
-	if obj_speed < throw_speed:
-		held_object.linear_velocity += throw_vec*(throw_speed - obj_speed)
+	if "linear_velocity" in held_object:
+		var obj_speed = held_object.linear_velocity.length()
+		if obj_speed < throw_speed:
+			held_object.linear_velocity += throw_vec*(throw_speed - obj_speed)
+	elif "velocity" in held_object:
+		var obj_speed = held_object.velocity.length()
+		if obj_speed < throw_speed:
+			held_object.velocity += throw_vec*(throw_speed - obj_speed)
 	held_object = null
 	
-func follow():
+func follow(delta):
 	if holder != null:
 		var a = phys_body.global_position
 		var b = holder.hm.hold_point.global_position
-		phys_body.linear_velocity = (b-a) * held_object_speed
+		if "linear_velocity" in phys_body:
+			phys_body.linear_velocity = (b-a) * held_object_speed * delta
+		if "velocity" in phys_body:
+			phys_body.velocity = (b-a) * held_object_speed * delta
---
+++ scenes/static_objects/grav_cannon.gd
@@ -0,0 +1,30 @@
+extends StaticBody3D
+
+@onready var hm = $HoldManager
+@onready var grab_range: Area3D = $GrabRange
+@onready var throw_timer: Timer = $ThrowTimer
+@onready var hold_point = $HoldPoint
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	grab_range.body_entered.connect(grab_object)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#print(hm.held_object)
+	pass
+
+
+func grab_object(object):
+	if hm.held_object == null:
+		hm.pick_up(object)
+		throw_timer.start()
+
+
+
+func _on_throw_timer_timeout():
+	hm.throw((hold_point.global_position - global_position).normalized())
+	pass # Replace with function body.
