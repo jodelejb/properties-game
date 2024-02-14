edd791e5a98f6676a0c4913a6f5795063a7cc128
Justas Jodele <jodele.justas@gmail.com>
2024-02-14 06:30:51 -0600

    Added switch platform

---
+++ scenes/static_objects/switch_platform.gd
@@ -0,0 +1,26 @@
+extends PathFollow3D
+
+@export var buttons: Array[InteractableButton]
+@export var speed: float = 3
+
+var active: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for btn in buttons:
+		btn.status_changed.connect(update_status)
+	update_status()
+	
+func update_status():
+	active = true
+	for btn in buttons:
+		if not btn.active: active = false
+		
+func _process(delta):
+	if active:
+		progress_ratio = lerp(progress_ratio,1.0,speed*delta)
+		#progress_ratio = 1
+		print(progress_ratio)
+	else:
+		progress_ratio = lerp(progress_ratio,0.0,speed*delta)
+		#progress_ratio = 0
